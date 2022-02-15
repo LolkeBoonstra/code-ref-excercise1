82c04fec318784bd2813ab91fcede6a285da7cc2
Sjors Hijgenaar <sjorshijgenaar@gmail.com>
2022-02-15 12:13:54 +0100

    added documentation to functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
+# function that takes two numbers as params
 def add(a,b):
+    # return the sum of two input numbers
     ret = a + b
     return ret
 
+# function that simply says hello in terminal
 def say_hello():
     print("Hello world!")
